bf00f902d3a0cb6e2f4df003b1ba68f12fbf0d13
yaelHB <87486870+yaelHB@users.noreply.github.com>
2021-07-15 10:22:31 -0500

    Create editor.java

---
+++ editor.java
@@ -0,0 +1,15 @@
+public class HolaMundo{
+
+}
+public class HolaMundo{
+  public satatic void main(String[] args){
+    
+  }
+}
+public class HolaMundo{
+  
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+
+}

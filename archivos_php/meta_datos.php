13555e6eea94044f0ba4d8308d946e8aa903e276
yaelHB <87486870+yaelHB@users.noreply.github.com>
2021-07-19 10:24:13 -0500

    Create meta_datos.php

---
+++ archivos_php/meta_datos.php
@@ -0,0 +1,12 @@
+<?php
+$titulo_sitio = "Registro Simple de Usuarios";
+$author = "Seminario de Titulación 2021 cecytem tx.";
+$viewport = "width=device-width, initial-scale=1.0";
+$keywords = "registro, registro simple, registro simple de usuarios";
+$description = "Registro Simple de Usuarios con PHP y MySQL";
+
+$meta_nombre1 = "author";
+$meta_nombre2 = "viewport";
+$meta_nombre3 = "keywords";
+$meta_nombre4 = "description";
+?>

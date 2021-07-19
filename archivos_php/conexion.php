96ddff0d9680fc35d9986540333b01a5ff94e612
yaelHB <87486870+yaelHB@users.noreply.github.com>
2021-07-19 10:20:44 -0500

    Create conexion.php

---
+++ archivos_php/conexion.php
@@ -0,0 +1,10 @@
+  
+<?php
+define('DB_SERVER', 'nombre_del_servidor');
+define('DB_USER', 'nombre_del_usuario');
+define('DB_PASSWD', 'contraseña_del_usuario');
+define('DB_NAME', 'nombre_de_la_base_de_datos');
+
+$conexion = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWD, DB_NAME) or die('No se pudo conectarse con la Base de Datos');
+mysqli_set_charset($conexion, "utf8");
+?>
